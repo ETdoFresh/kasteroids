1530e6c6f8a949d95436c4aaedfee60518e7c528
ETdoFresh <ETdoFresh@gmail.com>
2020-08-25 08:44:50 -0500

    Add "Enter to Type" for console window. Close #81

--- entities/ui/in_game_console.gd
+++ entities/ui/in_game_console.gd
@@ -1,16 +1,30 @@
 class_name InGameConsole
 extends Panel
 
+signal submitted_line(line)
+
 export var max_lines = 100
 
 var lines = []
 
+onready var line_edit = $VBoxContainer/LineEdit
+onready var instructions = $VBoxContainer/Instructions
+
 func _ready():
-    var _1 = $VBoxContainer/LineEdit.connect("text_entered", self, "submit_line")
+    var _1 = line_edit.connect("text_entered", self, "submit_line")
+
+func _input(event):
+    if event.is_action_pressed("ui_accept"):
+        if not line_edit.visible:
+            focus_line_edit()
 
 func submit_line(line):
-    $VBoxContainer/LineEdit.clear()
+    line_edit.clear()
     write_line(line)
+    line_edit.visible = false
+    instructions.visible = true
+    line_edit.release_focus()
+    emit_signal("submitted_line", line)
 
 func write_line(message):
     lines.append(message)
@@ -27,3 +41,9 @@ func write_line(message):
     $VBoxContainer/ScrollContainer/Label.text = text
     yield(get_tree(), "idle_frame")
     $VBoxContainer/ScrollContainer.scroll_vertical = $VBoxContainer/ScrollContainer.get_v_scrollbar().max_value
+
+func focus_line_edit():
+    line_edit.visible = true
+    instructions.visible = false
+    yield(get_tree(), "idle_frame")
+    line_edit.grab_focus()
