cb9decaaccf812966832397fd956da066bafae43
ETdoFresh <ETdoFresh@gmail.com>
2020-09-05 15:38:15 -0500

    Putting many hours into one file. Haha, well, modularizing and making predicted_world the best it can be. With queued creation/deletions and accurate world miss handling. Still lots to do, but progress is being made!

--- entities/world/predicted_world.gd
+++ entities/world/predicted_world.gd
@@ -4,7 +4,6 @@ export var enable = true
 export var smoothing_rate = 0.15
 
 var tick = 0
-var objects = []
 var ship_id = -1
 var input = InputData.new()
 var server_tick_sync
@@ -15,10 +14,11 @@ var types = {
     "Ship": Scene.SHIP,
     "Asteroid": Scene.ASTEROID,
     "Bullet": Scene.BULLET }
-var creation_list = []
-var deletion_list = []
-var remove_from_tree_queue = []
-var local_id = 1
+var objects = []
+var creation_queue = []
+var deletion_queue = []
+var awaiting_delete_confirmation = []
+var awaiting_create_confirmation = []
 
 onready var collision_manager = $CollisionManager
 onready var screen_size = get_viewport().get_visible_rect().size 
@@ -26,68 +26,44 @@ onready var containers = {
     "Ship": $Ships, "Asteroid": $Asteroids, "Bullet": $Bullets }
 
 func simulate(delta):
-    if received_data:
-        process_received_data()
-    
-    if server_tick_sync.smooth_tick_rounded <= tick:
+    process_received_data(delta)
+    if not is_new_tick(): return
+    tick = server_tick_sync.smooth_tick_rounded
+    simulate_predicted_world(delta)
+
+func simulate_predicted_world(delta):
+    update_inputs()
+    simulate_objects(delta)
+    record()
+    remove_objects_from_tree()
+    create_objects()
+
+func process_received_data(delta):
+    if not received_data:
         return
     
-    tick = server_tick_sync.smooth_tick_rounded
-    input.record(tick)
-    var ship = lookup(objects, "id", ship_id)
-    if ship: 
-        ship.input = input
-    for object in objects:
-        object.simulate(delta)
-        object.record(tick)
-    for item in creation_list:
-        if item.object && not objects.has(item.object):
-            item.object.simulate(delta)
-            item.object.record(tick)
-    collision_manager.resolve()
-    remove_queued_from_tree()
-
-func process_received_data():
     if received_data.tick < last_received_tick:
         return
     else:
         last_received_tick = received_data.tick
    
     ship_id = received_data.client.ship_id
-    create_new_objects(received_data)
-    remove_deleted_objects(received_data)
-    
-    for i in range(creation_list.size() - 1, -1, -1):
-        if creation_list[i].tick < received_data.tick:
-            if creation_list[i].id == -1:
-                if creation_list[i].object:
-                    creation_list[i].object.queue_free()
-            creation_list.remove(i)
-    
-    for i in range(deletion_list.size() - 1, -1, -1):
-        if deletion_list[i].tick < received_data.tick:
-            deletion_list.remove(i)
     
     var is_miss = false
-    for object in objects:
-        if object.id == -1:
-            continue
-        if not object.history.has(received_data.tick):
-            is_miss = true
-            break
-        else:
-            var historic_object = null
-            if object.has_node("History"):
-                historic_object = object.history.history[received_data.tick]
-            else:
-                historic_object = object.history[received_data.tick]
-            var other_object = lookup(received_data.objects, "id", object.id)
-            if not other_object:
-                continue
-            var delta = get_delta(historic_object, other_object)
-            if delta.position.length() >= 2 || delta.rotation >= 0.05:
-                is_miss = true
-                break
+    for entry in received_data.objects:
+        is_miss |= is_entry_deleted(entry, received_data.tick)
+        is_miss |= is_entry_not_predicted(entry)
+        if is_miss: break
+    
+    if not is_miss:
+        for object in objects:
+            is_miss |= is_object_exists_with_no_entry(object, received_data)
+            if is_miss: break
+    
+    if not is_miss:
+        for entry in received_data.objects:
+            is_miss |= mismatch_exceeds_threshold(entry)
+            if is_miss: break
     
     for object in objects:
         object.erase_history(received_data.tick)
@@ -103,14 +79,117 @@ func process_received_data():
             if correct_values:
                 object.from_dictionary(correct_values)
         #to_log("Resimulate", historical_state2.tick, historical_state2.input, historical_state2.objects)
+        world.rewind(received_data.tick)
+        world.rewrite(received_data)
         for resimulate_tick in range(received_data.tick + 1, server_tick_sync.smooth_tick + 1):
-            input.rewind(resimulate_tick)
-            var ship = lookup(objects, "id", ship_id)
-            if ship:
-                ship.input = input
-            for object in objects:
-                object.simulate(Settings.tick_rate)
-                object.record(resimulate_tick)
+            world.rewind(resimulate_tick)
+            simulate_predicted_world(delta)
+    
+    confirm_created_objects(received_data)
+    confirm_deleted_objects()
+    received_data = null
+
+func is_new_tick():
+    return server_tick_sync.smooth_tick_rounded > tick
+
+func update_inputs():
+    var ship = lookup(objects, "id", ship_id)
+    if ship: 
+        ship.input = input
+
+func simulate_objects(delta):
+    for object in objects:
+        object.simulate(delta)
+    collision_manager.resolve()
+
+func record():
+    input.record(tick)
+    for object in objects:
+        object.record(tick)
+
+func remove_objects_from_tree():
+    for i in range(deletion_queue.size() - 1, -1, -1):
+        var object = deletion_queue[i]
+        var container = object.get_parent()
+        object.get_parent().remove_child(object)
+        deletion_queue.remove(i)
+        awaiting_delete_confirmation.append({"tick": tick, "id": object.id, "object": object, "container": container})
+
+func create_objects():
+    for i in range(creation_queue.size() - 1, -1, -1):
+        var object = creation_queue[i].object
+        var container = creation_queue[i].container
+        object.collision_layer = Data.get_physics_layer_id_by_name("predicted_world")
+        object.collision_mask = Data.get_physics_layer_id_by_name("predicted_world")
+        object.connect("tree_exited", self, "erase_object", [object])
+        objects.append(object)
+        container.add_child(object)
+        object.physics.collision_manager = collision_manager
+        creation_queue.remove(i)
+        awaiting_create_confirmation.append({"tick": tick, "object": object})
+
+func mismatch_exceeds_threshold(entry):
+    var history = null
+    var object = List.lookup(objects, "id", entry.id)
+    if not object:
+        return false
+    if object.has_node("History"):
+        history = object.history.history[received_data.tick]
+    elif not object.history.has(received_data.tick):
+        history = object.history[received_data.tick]
+    else:
+        return true
+    
+    var delta = get_delta(historic_object, other_object)
+    if delta.position.length() >= 2 || delta.rotation >= 0.05:
+        return true
+    else:
+        return false
+
+func is_entry_deleted(entry, received_tick):
+    var awaiting_deletion = List.lookup(awaiting_delete_confirmation, "id", entry.id)
+    if awaiting_deletion:
+        if awaiting_deletion.tick < received_tick:
+            return true
+    return false
+
+func is_entry_not_predicted(entry):
+    for i in range(awaiting_create_confirmation.size()):
+        var awaiting_creation = awaiting_create_confirmation[i]
+        var type = types[entry.type]
+        if awaiting_creation.object is type:
+            if not "predicted" in awaiting_creation:
+                awaiting_creation["predicted"] = true
+                return false
+    return true
+
+func is_object_exists_with_no_entry(object, received_data):
+    if object.id == -1:
+        return false
+    if List.lookup(received_data.objects, "id", object.id):
+        return true
+    else:
+        return false
+
+func create_object(entry):
+    return true
+
+func confirm_created_objects(received_data):
+    for entry in received_data.objects:
+        if not List.lookup(objects, "id", entry.id):
+            for i in range(awaiting_create_confirmation.size()):
+                var awaiting_creation = awaiting_create_confirmation[i]
+                var type = types[entry.type]
+                if awaiting_creation.object is type:
+                    awaiting_creation.object.id = entry.id
+                    awaiting_create_confirmation.remove(i)
+                    break
+
+func confirm_deleted_objects():
+    for i in range(awaiting_delete_confirmation.size()):
+        var awaiting_deletion = awaiting_delete_confirmation[i]
+        awaiting_deletion.object.queue_free()
+        awaiting_delete_confirmation.remove(i)
 
 func get_delta(source, target):
     var delta = {}
@@ -161,14 +240,14 @@ func create_new_objects(dictionary):
     for entry in dictionary.objects:
         if entry:
             var object = lookup(objects, "id", entry.id)
-            var on_deletion_list = lookup(deletion_list, "id", entry.id)
-#            if on_deletion_list && dictionary.tick > on_deletion_list.tick:
-#                on_deletion_list.object.recreate()
-#                on_deletion_list.object.from_dictionary(entry)
-#                objects.append(on_deletion_list.object)
-#                deletion_list.erase(on_deletion_list)
+            var on_deletion_queue = lookup(deletion_queue, "id", entry.id)
+#            if on_deletion_queue && dictionary.tick > on_deletion_queue.tick:
+#                on_deletion_queue.object.recreate()
+#                on_deletion_queue.object.from_dictionary(entry)
+#                objects.append(on_deletion_queue.object)
+#                deletion_queue.erase(on_deletion_queue)
 #            elif ...
-            if not object && not on_deletion_list:
+            if not object && not on_deletion_queue:
                 create_object(entry)
 
 func remove_deleted_objects(dictionary):
@@ -178,37 +257,18 @@ func remove_deleted_objects(dictionary):
             objects.remove(i)
             object.queue_free()
     
-    for i in range(deletion_list.size() - 1, -1, -1):
-        var delete_item = deletion_list[i]
+    for i in range(deletion_queue.size() - 1, -1, -1):
+        var delete_item = deletion_queue[i]
         if not lookup(dictionary.objects, "id", delete_item.id):
             if delete_item.object:
                 delete_item.object.queue_free()
-            deletion_list.remove(i)
-
-func create_object(entry):
-    var type = entry.type
-    var object = claim_new_object(entry)
-    if not object:
-        object = types[type].instance()
-        object.collision_layer = Data.get_physics_layer_id_by_name("predicted_world")
-        object.collision_mask = Data.get_physics_layer_id_by_name("predicted_world")
-        object.connect("tree_exited", self, "erase_object", [object])
-        objects.append(object)
-        containers[type].add_child(object)
-        if "physics" in object: object.physics.collision_manager = collision_manager
-        object.from_dictionary(entry)
-        if type == "Bullet" && "ship_id" in entry:
-            var ship = lookup(objects, "id", ship_id)
-            if ship:
-                object.add_collision_exception_with(ship)
-        if object.has_signal("bullet_created"):
-            object.connect("bullet_created", self, "create_bullet")
+            deletion_queue.remove(i)
 
 func erase_object(object):
-    var created_object = lookup(creation_list, "object", object)
+    var created_object = lookup(creation_queue, "object", object)
     var created_local_id = created_object.local_id if created_object else -1
     CSV.write_line("res://object.csv", ["p_erase",tick,object.id,created_local_id,object.position,object.rotation])
-    deletion_list.append({"tick": tick, "id": object.id, "local_id": created_local_id, "object": object})
+    deletion_queue.append({"tick": tick, "id": object.id, "local_id": created_local_id, "object": object})
     objects.erase(object)
 
 func lookup(list, key, value):
@@ -228,7 +288,7 @@ func to_dictionary():
 
 func claim_new_object(entry):
     var closest = { "distance": 1000000, "unclaimed": null }
-    for unclaimed in creation_list:
+    for unclaimed in creation_queue:
         if unclaimed.id == -1:
             var distance = (entry.create_position - unclaimed.create_position).length()
             if distance < closest.distance:
@@ -236,7 +296,7 @@ func claim_new_object(entry):
     
     if closest.unclaimed:
         closest.unclaimed.id = entry.id
-        var deleted_object = lookup(deletion_list, "local_id", closest.unclaimed.local_id)
+        var deleted_object = lookup(deletion_queue, "local_id", closest.unclaimed.local_id)
         if deleted_object:
             deleted_object.id = entry.id
         
@@ -262,7 +322,7 @@ func create_bullet(bullet):
     $Bullets.add_child(bullet)
     bullet.physics.collision_manager = collision_manager
     bullet.record(tick)
-    creation_list.append({"tick": tick, "id": -1, "local_id": local_id, "object": bullet, "create_position": bullet.global_position})
+    creation_queue.append({"tick": tick, "id": -1, "local_id": local_id, "object": bullet, "create_position": bullet.global_position})
     CSV.write_line("res://object.csv", ["p_create",tick,bullet.id,local_id-1,bullet.position,bullet.rotation])
     debug_bullet_create(bullet)
 
@@ -275,7 +335,3 @@ func debug_bullet_destroy(_bullet):
 
 func queue_remove_from_tree(child):
     remove_from_tree_queue.append(child)
-
-func remove_queued_from_tree():
-    for node in remove_from_tree_queue:
-        node.get_parent().remove_child(node)
