9a2d61c4fe3ae53c8111faefd9f084cc5f21123d
ETdoFresh <ETdoFresh@gmail.com>
2020-09-13 23:54:58 -0500

    Remove "travel" and change shape functions to use "collide" instead of "collide_with_motion"

--- functions/physics/physics.gd
+++ functions/physics/physics.gd
@@ -7,8 +7,8 @@ const PHYSICS_NODES = PhysicsNodesFunctions
 static func simulate(objects: Array, delta: float) -> Array:
     objects = objects.duplicate()
     objects = LIST.map(objects, funcref(COLLISION, "clear_collisions"))
-    objects = LIST.map(objects, funcref(PHYSICS_NODES, "update_physical_body")) # Side-effect
     objects = LIST.map1(objects, funcref(PHYSICS_NODES, "move"), delta)
+    objects = LIST.map(objects, funcref(PHYSICS_NODES, "update_physical_body")) # Side-effect
     objects = LIST.map1(objects, funcref(PHYSICS_NODES, "collide"), objects)
     objects = LIST.map(objects, funcref(COLLISION, "fix_penetration"))
     objects = LIST.map(objects, funcref(COLLISION, "bounce_no_angular_velocity"))
--- functions/physics/physics_nodes.gd
+++ functions/physics/physics_nodes.gd
@@ -14,7 +14,6 @@ static func move(object : Dictionary, delta : float) -> Dictionary:
     if not "position" in object: return object
     if not "rotation" in object: return object
     object = object.duplicate()
-    object["travel"] =  object.linear_velocity * delta
     object.position += object.linear_velocity * delta
     object.rotation += object.angular_velocity * delta
     return object
@@ -26,7 +25,6 @@ static func collide(object : Dictionary, other_objects : Array) -> Dictionary:
     for collision_shape_2d in object.node.collision_shapes_2d:
         var shape = collision_shape_2d.shape
         var transform = collision_shape_2d.global_transform
-        var motion = object.travel
         for other_object in other_objects:
             if not "node" in other_object: continue
             if not "collision_shapes_2d" in other_object.node: return other_object
@@ -34,18 +32,17 @@ static func collide(object : Dictionary, other_objects : Array) -> Dictionary:
             for other_collision_shape_2d in other_object.node.collision_shapes_2d:
                 var other_shape = other_collision_shape_2d.shape
                 var other_transform = other_collision_shape_2d.global_transform
-                var other_motion = other_object.travel
-                if collide_with_motion(shape, transform, motion, other_shape, other_transform, other_motion):
+                if did_collide(shape, transform, other_shape, other_transform):
                     object["collisions"].append(get_collision_data(other_object,
-                        shape, transform, motion, other_shape, other_transform, other_motion))
+                        shape, transform, other_shape, other_transform))
                     break # Only consider one collision per pair of objects
     return object
 
-static func collide_with_motion(shape, transform, motion, other_shape, other_transform, other_motion):
-    return shape.collide_with_motion(transform, motion, other_shape, other_transform, other_motion)
+static func did_collide(shape, transform, other_shape, other_transform):
+    return shape.collide(transform, other_shape, other_transform)
 
-static func get_collision_data(other_object, shape, transform, motion, other_shape, other_transform, other_motion):
-    var contacts = shape.collide_with_motion_and_get_contacts(transform, motion, other_shape, other_transform, other_motion)
+static func get_collision_data(other_object, shape, transform, other_shape, other_transform):
+    var contacts = shape.collide_and_get_contacts(transform, other_shape, other_transform)
     var average_contact = Vector2.ZERO
     for contact in contacts: average_contact += contact
     average_contact /= contacts.size()
